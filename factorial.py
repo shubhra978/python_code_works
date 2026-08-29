c9cf45b2961fef8f5bca807f5ad19291100e8242
SHUBHRANIL ROY <shubhranil96@gmail.com>
2026-08-29 12:54:41 +0530

    Refactor factorial and add sum_of_numbers function
    Refactored factorial function and added sum_of_numbers function.

--- factorial.py
+++ factorial.py
@@ -1,8 +1,15 @@
-def factorial(n):
-  if n==0:
-    return 1
-  else:
-    return n*factorial(n-1)
-  return -1
-
-factorial(n)
+def factorial(num):
+    if(num==1): #base case
+        return 1
+    else:
+        return num * factorial(num-1) #recursion function call
+    
+def sum_of_numbers(num):
+    if(num==1): #base case
+        return 1
+    else:
+        return num + factorial(num-1) #recursion function call
+    
+number = 5
+print(factorial(number))
+print(sum_of_numbers(number))
