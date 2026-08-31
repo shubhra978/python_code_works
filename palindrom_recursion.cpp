ffa95e96db41897caf811a03a0de3ee282531207
SHUBHRANIL ROY <shubhranil96@gmail.com>
2026-08-31 23:59:04 +0530

    Implement recursive palindrome checker
    Fixes syntax errors and implements palindrome check using recursion.

---
+++ palindrom_recursion.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool palindrome(str my_string, int index){
+    
+    if(my_string.length() ==0){
+        return true;
+    }
+    else if(my_string[0]!=my_string[index]{
+        return false;
+    }
+    else{
+        palindrome(str my_string, int index)
+    }
+    
+}
+
+
+int main() {
+	str one = "hello";
+	int index = one.length();
+	palindrome(one,index-1)
+	
+	
+}
