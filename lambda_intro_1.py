900083141979e17b7c605261fbb98c827a7f5c97
SHUBHRANIL ROY <shubhranil96@gmail.com>
2026-09-02 23:23:05 +0530

    Refactor lambda function examples in lambda_intro_1.py
    Updated lambda function usage and added sorting and mapping examples.

--- lambda_intro_1.py
+++ lambda_intro_1.py
@@ -4,4 +4,15 @@ print(result(7))
 
 new_result = lambda a, b: a * b
 
-print(new_result(8,9))
+print(new_result(7,8))
+
+
+
+names = ["alice", "bob", "charlie"]
+Upper_list =  list(map(lambda n : n.upper(),names))
+
+print(Upper_list)
+
+
+sorted_list = sorted(names,key=lambda sizecheck: len(sizecheck))
+print(sorted_list)
